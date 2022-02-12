8fc188c77e54540e51d7e85248e4b53ccfa176c7
Alex-Mercy <hambiru@yandex.ru>
2022-02-12 18:03:47 +0300

    build

---
+++ build/precache-manifest.0374426aa30680cfcf161b72c50e6d67.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6388ebf52bbea16b132f7c2ca7e5d95",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "b7fc2c4f1828e728689d",
+    "url": "/to-do-list/static/js/2.04b38954.chunk.js"
+  },
+  {
+    "revision": "8c11228ffb9b6ece10e5",
+    "url": "/to-do-list/static/js/main.5789bcd3.chunk.js"
+  },
+  {
+    "revision": "c5b49eebbffb21c94adb",
+    "url": "/to-do-list/static/js/runtime-main.6169b897.js"
+  }
+]);
